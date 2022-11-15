a2e2ef064f97f821162f57122084e0255e60bf7d
Eder Correia <ederco77@gmail.com>
2022-11-15 15:20:12 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ederco/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.ederco.workshopmongo.services;
+
+import com.ederco.workshopmongo.domain.User;
+import com.ederco.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
