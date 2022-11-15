f8f78595ff9bd1cdccc543c6ea1079191d8d66ed
Eder Correia <ederco77@gmail.com>
2022-11-15 19:06:03 -0300

    Inserção de usuário com POST

--- src/main/java/com/ederco/workshopmongo/services/UserService.java
+++ src/main/java/com/ederco/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ederco.workshopmongo.services;
 
 import com.ederco.workshopmongo.domain.User;
+import com.ederco.workshopmongo.dto.UserDTO;
 import com.ederco.workshopmongo.repository.UserRepository;
 import com.ederco.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,12 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
