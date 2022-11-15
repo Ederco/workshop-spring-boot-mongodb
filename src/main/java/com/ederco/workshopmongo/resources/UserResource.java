118365f494ce631a33c8319b5f53ff11471ff8d2
Eder Correia <ederco77@gmail.com>
2022-11-15 17:41:00 -0300

    obtendo um usuário por id

--- src/main/java/com/ederco/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ederco/workshopmongo/resources/UserResource.java
@@ -1,16 +1,15 @@
-package com.ederco.workshopmongo.resource;
+package com.ederco.workshopmongo.resources;
 
 import com.ederco.workshopmongo.domain.User;
 import com.ederco.workshopmongo.dto.UserDTO;
 import com.ederco.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,12 +21,17 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<UserDTO>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream()
                 .map(x -> new UserDTO(x))
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
 }
