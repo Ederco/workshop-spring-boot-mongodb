3d0d7c62577ff176d5be278c72ef96ffd8694ec1
Eder Correia <ederco77@gmail.com>
2022-11-15 21:38:18 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ederco/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.ederco.workshopmongo.services;
+
+import com.ederco.workshopmongo.domain.Post;
+import com.ederco.workshopmongo.domain.User;
+import com.ederco.workshopmongo.dto.UserDTO;
+import com.ederco.workshopmongo.repository.PostRepository;
+import com.ederco.workshopmongo.repository.UserRepository;
+import com.ederco.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
+
